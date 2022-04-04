f619d198da23874bb26405327c951440242d3886
PohleAnton <s0582388@htw-berlin.de>
2022-04-04 16:25:53 +0200

    test

---
+++ Klasse.java
@@ -0,0 +1,14 @@
+package myapp;
+
+public class Klasse {
+
+    public int age;
+
+    public Klasse (int age){
+        this.age=age;
+    };
+
+    public int getAge(){
+        return this.age;
+    }
+}
